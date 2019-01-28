4f4e074bd3f776049d571ce3f1c4d5d888250b08
leo young <591014238@qq.com>
2019-01-28 11:46:39 +0800

    second commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,113 +1,170 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
+  <div class="index">
+    <div class="swiper-container">
+      <div class="swiper-wrapper">
+        <div v-for="item in 100" class="swiper-slide">
+          <img @click="showFull" src="../assets/index/1.jpg">
+        </div>
+      </div>
+      <!-- Add Pagination -->
+      <div class="swiper-pagination"></div>
+    </div>
+    <div>
+      <h1 class="animated bounceIn">测试文字</h1>
+    </div>
+    <transition name="fade">
+      <div id="full-screen" @click="hideFull" v-if="imgSrc">
+        <img :src="imgSrc">
+        <h3 class="slideInLeft" :class="{animated:imgSrc}">文字</h3>
+        <h3 class="slideInRight" :class="{animated:imgSrc}">文字</h3>
+      </div>
+    </transition>
   </div>
 </template>
-
+ 
 <script>
-export default {
-  name: 'HelloWorld',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
+  import Swiper from 'swiper';
+  import '../add/swiper.animate.min.js';
+  import '../add/swiper.min.css';
+  import '../add/animate.css';
+  
+  export default {
+    name: "index",
+    data() {
+      return {
+        imgSrc: ""
+      };
+    },
+    mounted: () => {
+      new Swiper('.swiper-container', {
+        slidesPerView: 2,
+        slidesPerColumn: 2,
+        spaceBetween: 6,
+        freeMode: true,
+        pagination: {
+          el: '.swiper-pagination',
+          type: 'progressbar',
+          clickable: true,
+        },
+      })
+    },
+    methods: {
+      showFull: function(event) {
+        this.imgSrc = event.target.src;
+      },
+      hideFull: function() {
+        this.imgSrc = "";
+      }
     }
-  }
-}
+  };
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+<style scoped lang="scss">
+  * {
+    padding: 0;
+    margin: 0;
+  }
+  
+  html,
+  body,
+  #app,
+  #full-screen,
+  .index {
+    position: absolute;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
+  }
+  
+  #full-screen {
+    z-index: 100;
+  }
+  
+  body {
+    background: #eee;
+    font-family: Helvetica Neue, Helvetica, Arial, sans-serif;
+    font-size: 14px;
+    color: #000;
+    margin: 0;
+    padding: 0;
+  }
+  
+  .swiper-container {
+    width: 100%;
+    height: auto;
+    margin-left: auto;
+    margin-right: auto;
+  }
+  
+  .swiper-slide {
+    text-align: center;
+    font-size: 18px;
+    background: #fff;
+    height: 230px;
+    /* Center slide text vertically */
+    display: -webkit-box;
+    display: -ms-flexbox;
+    display: -webkit-flex;
+    display: flex;
+    -webkit-box-pack: center;
+    -ms-flex-pack: center;
+    -webkit-justify-content: center;
+    justify-content: center;
+    -webkit-box-align: center;
+    -ms-flex-align: center;
+    -webkit-align-items: center;
+    align-items: center;
+    box-shadow: 1px 1px 3px #999;
+  }
+  
+  .swiper-slide img,
+  #full-screen img {
+    float: left;
+    width: 100%;
+    height: 100%;
+  }
+  
+  #full-screen h3 {
+    position: absolute;
+    box-sizing: border-box;
+    width: 100%;
+    padding: 0 16px;
+    color: white;
+    font-size: 40px;
+    text-align: left;
+    &:hover {
+      color: #000;
+    }
+    &.slideInLeft {
+      top: 100px;
+    }
+    &.slideInRight {
+      top: 300px;
+      text-align: right;
+    }
+  }
+  
+  .fade-enter-active,
+  .fade-leave-active {
+    transition: all 0.5s;
+  }
+  
+  .fade-enter,
+  .fade-leave {
+    transform: translate3d(60px, 0, 0);
+    opacity: 0;
+  }
+  
+  .faderight-enter-active,
+  .faderight-leave-active {
+    transition: all 1.8s;
+  }
+  
+  .faderight-enter,
+  .faderight-leave {
+    transform: translate3d(400px, 0, 0);
+    opacity: 0;
+  }
 </style>
